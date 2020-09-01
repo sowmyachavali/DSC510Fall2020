f0a05c3f316531bc404ee12008371b69d0edd943
Aditya Sumbaraju <aditya.sumbaraju@bluenile.com>
2020-09-01 13:39:52 -0700

    1.2 Assignment DSC510 by Aditya Sumbaraju

---
+++ SUMBARAJU_DSC510/Hello_World.py
@@ -0,0 +1,10 @@
+# wk1_DSC510-T303_1.1_Assignment
+# print('hello world')  # program to print statement
+
+def print_myfirstcode(str):
+    print(f'Hello {str}')
+
+
+if __name__ == '__main__':
+    print_myfirstcode('World')
+# End of program
